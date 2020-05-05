10667909eeb6c7d52bcca55fc44e370003627d79
Oleksiy Oletsky <oletsky@ukr.net>
2020-05-05 21:45:33 +0300

    Add files via upload

---
+++ GenericArrayFunction.java
@@ -0,0 +1,20 @@
+package javatraining.oletsky.arrays;
+
+/**
+ * author O.Oletsky
+ * Generic function with arrays
+ */
+
+public class GenericArrayFunction {
+    public static void main(String[] args) {
+        System.out.println
+                (GenericArrayFunction.getMiddle
+                        (new Integer[] {5, 10, 8, 3, 1})
+                );
+    }
+
+    static <T> T getMiddle (T[] m)
+    {
+        return m[m.length/2];
+    }
+}
